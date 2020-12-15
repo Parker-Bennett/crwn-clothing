4f0d5e8a5900074103eb5befad6bb7d4c17137d2
Parker Bennett <parker.benn@live.com>
2020-12-14 21:54:17 -0500

    adding header and shop page with collection previews

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import "./App.css";
 import { Switch, Route } from "react-router-dom";
 
+import Header from "./components/header/header.component";
 import Homepage from "./pages/homepage/homepage.component";
-
-function hatspages() {
-  return (
-    <div className="">
-      <h1>HATS PAGE</h1>
-    </div>
-  );
-}
+import ShopPage from "./pages/shop/shop.component";
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
         <Route exact path="/" component={Homepage} />
-        <Route path="/shop/hats" component={hatspages} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
